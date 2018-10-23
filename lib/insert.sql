f398df35c0360f5eadeb80ba75481e6b2d620b74
larryvsdata <larryvsdata@gmail.com>
2018-10-22 20:40:14 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO BEARS (ID , NAME , AGE , GENDER , COLOR , temperament, ALIVE) VALUES (1, "Mr. Chocolate" , 1 , "Male" , "WHITE" , "MILD" , 1);
+INSERT INTO BEARS (ID , NAME , AGE , GENDER , COLOR , temperament, ALIVE) VALUES (2, "Rowdy" , 2 , "Male" , "WHITE" , "TOUGH" , 1);
+INSERT INTO BEARS (ID , NAME , AGE , GENDER , COLOR , temperament, ALIVE) VALUES (3, "Tabitha" , 3 , "Female" , "WHITE" , "TOUGH" , 0);
+INSERT INTO BEARS (ID , NAME , AGE , GENDER , COLOR , temperament, ALIVE) VALUES (4, "Sergeant Brown" , 4 , "Male" , "WHITE" , "TOUGH" , 1);
+INSERT INTO BEARS (ID , NAME , AGE , GENDER , COLOR , temperament, ALIVE) VALUES (5, "Melissa" , 5 , "Female" , "WHITE" , "MILD" , 1);
+INSERT INTO BEARS (ID , NAME , AGE , GENDER , COLOR , temperament, ALIVE) VALUES (6, "Grinch" , 6 , "Male" , "WHITE" , "MILD" , 0);
+INSERT INTO BEARS (ID , NAME , AGE , GENDER , COLOR , temperament, ALIVE) VALUES (7, "Wendy" , 7 , "Female" , "WHITE" , "MILD" , 1);
+INSERT INTO BEARS (ID  , AGE , GENDER , COLOR , temperament, ALIVE) VALUES (8 , 8 , "Male" , "WHITE" , "TOUGH" , 1);
